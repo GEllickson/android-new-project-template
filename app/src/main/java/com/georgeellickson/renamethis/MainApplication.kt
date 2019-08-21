f9a44e07431c269418b03cf0312015c4ebe185fc
George Ellickson <GEllickson@users.noreply.github.com>
2019-08-20 19:18:46 -0700

    MainApplication class

---
+++ app/src/main/java/com/georgeellickson/renamethis/MainApplication.kt
@@ -0,0 +1,15 @@
+package com.georgeellickson.renamethis
+
+import android.app.Application
+
+class MainApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        // init anything core
+
+        // init Dagger
+
+        // init 3rd party libs
+    }
+}
