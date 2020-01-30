ef62537e161cdf0e5a68733af5b024de93c9c598
George Ellickson <GEllickson@users.noreply.github.com>
2020-01-30 14:15:31 -0800

    Bash script to copy project

---
+++ copy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+    echo "Please provide the name of the new app directory"
+    exit 1
+fi
+
+NEW_APP_DIR="$1"
+PROJECT_NAME=$(basename ~+)
+
+mkdir ../$NEW_APP_DIR
+cp -r app gradle .gitignore build.gradle gradle.properties gradlew gradlew.bat settings.gradle ../$NEW_APP_DIR
